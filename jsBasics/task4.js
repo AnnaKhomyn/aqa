7cf5f7310439acdaf07b1083b44556b42ef9d6ba
Anna Khomyn <anyakhomun@gmail.com>
2024-11-05 23:47:02 +0200

    JavaScript primitive data types

---
+++ jsBasics/task4.js
@@ -0,0 +1,21 @@
+//4.1
+const radius = 4;
+
+const circleArea = Math.PI * radius ^ 2;
+
+console.log(circleArea.toFixed(2))
+
+//4.2
+const length = 3;
+const width = 2;
+
+const rectangleArea = length * width;
+
+console.log(rectangleArea.toFixed(2))
+
+//4.3
+const height = 5;
+
+const cylinderArea = Math.PI * radius ^ 2 * height;
+
+console.log(cylinderArea.toFixed(2))
